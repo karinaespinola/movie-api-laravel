305c1b9f1068a489fbe70da6c1b0d27a0a1b40a0
Karina Espinola <k.espinolah@gmail.com>
2022-07-10 19:03:01 -0600

    Add more query parameters to the movie endpoint

--- app/Http/Controllers/Api/MoviesController.php
+++ app/Http/Controllers/Api/MoviesController.php
@@ -2,23 +2,31 @@
 
 namespace App\Http\Controllers\Api;
 
-use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Http;
 
 class MoviesController extends Controller
 {
-  private $requestUrl = 'https://api.themoviedb.org/3/discover/';  
+  private $requestUrl = 'https://api.themoviedb.org/3/discover/movie';  
   
   public function getSuggestions(Request $request) {
-      $media    = $request->media;
       $genre    = $request->genre;
       $cast     = $request->cast;
       $keywords = $request->keywords;
-      // Check if the media paratemer is present in the request
-      if (! $request->filled('media')) {
-        return response()->json([
-          "message" => "Please select a media type, Movies or TV."
-        ], 400);
-      }      
+      
+      // Make request to the TMDB
+      $response = Http::get($this->requestUrl, [
+        'api_key'       => env('TMDB_API_KEY', null), 
+        'with_genres'   => $genre,
+        'with_people'   => $cast,
+        'with_keywords' => $keywords,
+        'sort_by'       => 'popularity.desc',
+        'page'          => 1
+      ]);
+
+      return response()->json([
+        'data' => $response->json()
+      ], 200);
     }
 }
